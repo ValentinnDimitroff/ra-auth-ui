1d0b0ee0e83a485ceded5b7c3fa02a9e6f2b10ce
Valentin Dimitroff <v.dimitroff@yahoo.com>
2023-12-29 00:28:25 +0200

    Add demo project

---
+++ examples/demo/src/App.tsx
@@ -0,0 +1,26 @@
+import {
+  Admin,
+  Resource,
+  ListGuesser,
+  EditGuesser,
+  ShowGuesser,
+} from "react-admin";
+import { dataProvider } from "./dataProvider";
+import { authProvider } from "./authProvider";
+
+export const App = () => (
+  <Admin dataProvider={dataProvider} authProvider={authProvider}>
+    <Resource
+      name="posts"
+      list={ListGuesser}
+      edit={EditGuesser}
+      show={ShowGuesser}
+    />
+    <Resource
+      name="comments"
+      list={ListGuesser}
+      edit={EditGuesser}
+      show={ShowGuesser}
+    />
+  </Admin>
+);
