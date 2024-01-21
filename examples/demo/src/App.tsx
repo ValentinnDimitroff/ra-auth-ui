67cb470bef5cd305660d08f5f72c98c504a1f627
Valentin Dimitroff <v.dimitroff@yahoo.com>
2024-01-21 02:16:45 +0200

    Stabilze demo project

--- examples/demo/src/App.tsx
+++ examples/demo/src/App.tsx
@@ -1,39 +1,13 @@
 import {
-  Admin,
   Resource,
   ListGuesser,
   EditGuesser,
   ShowGuesser,
   defaultTheme,
-  CustomRoutes,
-  AdminProps,
 } from "react-admin";
 import { dataProvider } from "./dataProvider";
 import { authProvider } from "./authProvider";
 import { AuthAdmin } from "ra-auth-ui";
-import { FC, PropsWithChildren } from "react";
-import { Route } from "react-router-dom";
-// import { QueryClient, QueryClientProvider } from "react-query";
-
-// const queryClient = new QueryClient({
-//   defaultOptions: {
-//     queries: {
-//       retry: false,
-//       structuralSharing: false,
-//     },
-//     mutations: {
-//       retryDelay: 10000,
-//     },
-//   },
-// });
-
-const InnerApp: FC<AdminProps> = ({ children, ...rest }) => {
-  return (
-    <Admin loginPage={false} {...rest}>
-      {children}
-    </Admin>
-  );
-};
 
 export const App = () => {
   return (
