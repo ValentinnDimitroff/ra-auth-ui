9623d84376f0ed00b34333bcad9fde3096904c62
Valentin Dimitroff <v.dimitroff@yahoo.com>
2024-01-25 00:14:42 +0200

    Change formats

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import pkg from './package.json'
 
 export default defineConfig({
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.tsx'),
             name: 'ra-auth-ui',
-
             fileName: 'index',
+            formats: ['es', 'cjs'],
         },
         rollupOptions: {
-            external: ['react', 'react-dom', 'react-admin', 'react/jsx-runtime', '@mui/material'],
+            external: [...Object.keys(pkg.peerDependencies || {}), 'react/jsx-runtime'],
         },
     },
 })
