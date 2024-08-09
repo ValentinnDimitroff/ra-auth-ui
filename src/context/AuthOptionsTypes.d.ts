f5f8b0266bb7d584cf2a4d29b34b0b478858d765
batanoffs <batanoff.s@protonmail.com>
2024-08-09 16:53:24 +0300

    updated interface

--- src/context/AuthOptionsTypes.d.ts
+++ src/context/AuthOptionsTypes.d.ts
@@ -1,12 +1,8 @@
-// d:/01. IT CAREER/04. REAL PROJECTS/ra-auth-ui/src/context/AuthOptionsContext.d.ts
-
 import { FC, ReactNode } from 'react';
 
 export interface AuthOptionsContextValue {
-    authOptions: {
         profilePage?: ReactNode;
         userMenuItems?: ReactNode[];
-    };
 }
 
 declare const AuthOptionsContext: React.Context<AuthOptionsContextValue>;
