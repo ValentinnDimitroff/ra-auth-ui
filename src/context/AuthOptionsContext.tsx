6a82b69ab6f20d3542eec492701fd65e369f4c00
batanoffs <batanoff.s@protonmail.com>
2024-08-09 16:52:45 +0300

    removed props types and fixed context props

--- src/context/AuthOptionsContext.tsx
+++ src/context/AuthOptionsContext.tsx
@@ -1,12 +1,10 @@
 import React, { createContext } from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import { AuthOptionsContextValue } from './AuthOptionsTypes'
 
 const AuthOptionsContext = createContext<AuthOptionsContextValue>({
-    authOptions: {
         profilePage: null,
         userMenuItems: [],
-    },
 })
 
 AuthOptionsContext.displayName = 'AuthOptionsContext'
@@ -23,10 +21,10 @@ const AuthOptionsContextProvider = ({
     return <AuthOptionsContext.Provider value={value}>{children}</AuthOptionsContext.Provider>
 }
 
-AuthOptionsContextProvider.propTypes = {
-    value: PropTypes.object.isRequired,
-    children: PropTypes.node.isRequired,
-}
+// AuthOptionsContextProvider.propTypes = {
+//     value: PropTypes.object.isRequired,
+//     children: PropTypes.node.isRequired,
+// }
 
 export { AuthOptionsContext, AuthOptionsContextProvider }
 
