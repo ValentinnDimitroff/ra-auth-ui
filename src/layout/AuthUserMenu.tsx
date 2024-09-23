522ca88406e5b11415e8246ed5a7eb000531bd08
batanoffs <batanoff.s@protonmail.com>
2024-09-23 14:43:28 +0300

    Revert "added profilePage item into userMenu dropdown, complemented with conditional"
    This reverts commit a0585a1c083c77f205a89aa52f792f6d8e973673.

--- src/layout/AuthUserMenu.tsx
+++ src/layout/AuthUserMenu.tsx
@@ -1,29 +1,18 @@
 import { FC, useContext } from 'react'
-import { Logout, MenuItemLink, UserMenu } from 'react-admin'
+import { Logout, UserMenu } from 'react-admin'
 import { AuthOptionsContext } from '../context/AuthOptionsContext'
-import { Person } from '@mui/icons-material'
 
 /**
- * UserMenu constructed by authOptions' profilePage and userMenuItems
- * @returns
+ * UserMenu constructed by authOptions' profilePage and userMenuItems 
+ * @returns 
  */
 export const AuthUserMenu: FC = () => {
     const { userMenuItems, profilePage } = useContext(AuthOptionsContext)
-    
+
     return (
         <UserMenu>
-            {profilePage && (
-                <MenuItemLink
-                    to="/profile"
-                    leftIcon={<Person />}
-                    primaryText="Profile"
-                    placeholder={''}
-                />
-            )}
-
-            {userMenuItems?.map((item, key) => (
-                <li key={key}>{item}</li>
-            ))}
+            {/* {profilePage} */}
+            {userMenuItems}
             <Logout />
         </UserMenu>
     )
